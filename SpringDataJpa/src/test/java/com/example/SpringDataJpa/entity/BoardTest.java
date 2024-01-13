856a81b09a5b344343b6f1f12642d3b437c8b8f2
dev yoon <vhdhxh@naver.com>
2024-01-13 16:02:48 +0900

    Auditing

--- SpringDataJpa/src/test/java/com/example/SpringDataJpa/entity/BoardTest.java
+++ SpringDataJpa/src/test/java/com/example/SpringDataJpa/entity/BoardTest.java
@@ -17,6 +17,7 @@ class BoardTest {
     @Autowired
     BoardRepository boardRepository;
 
+
     @Test
     void 보드엔티티테스트() {
         Board board = Board.builder().title("제목").contents("내용").build();
